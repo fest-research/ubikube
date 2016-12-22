38b41cd07f636c035e55ece07f99bad244fe79f6
maciaszczykm <maciaszczykm@icloud.com>
2016-12-22 14:48:13 +0100

    Add background

--- src/app/fjtheme.js
+++ src/app/fjtheme.js
@@ -4,11 +4,12 @@ import {red600} from 'material-ui/styles/colors';
 export const fjTheme = getMuiTheme({
   appBar: {
     height: 50,
-    color: red600,
+    color: 'white',
   },
   palette: {
     primary1Color: red600,
     accent1Color: red600,
     textColor: red600,
+    backgroundColor: red600
   }
 });
