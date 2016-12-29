c159cf96a8f88e2cf027e0eeebac10448bb1e257
maciaszczykm <maciaszczykm@icloud.com>
2016-12-29 11:52:54 +0100

    Fix extraction of system image

--- src/app/scripts/unzip.js
+++ src/app/scripts/unzip.js
@@ -2,11 +2,16 @@ export function extract7z (archive, outputdir, callback) {
 
 var path = require('path')
 var exec = require('child_process').exec
+
+console.log(__dirname);
+console.log(__filename);
+console.log(process.cwd());
+
   if (process.platform == 'darwin') {
-    var binary = '"' + path.normalize(__dirname + '/../7z') + '"'
+    var binary = '"' + path.normalize(process.cwd() + '/7z') + '"'
   }
   else if (process.platform == 'win32') {
-    var binary = '"' + path.normalize(__dirname + '/../7z.exe') + '"'
+    var binary = '"' + path.normalize(process.cwd() + '/7z.exe') + '"'
   }
   else if (process.platform == 'linux') {
     var binary = '7za'
