1a685759ff5ab580c7c0db54344d2f58afa4e9f1
Marcin Maciaszczyk <marcin.maciaszczyk@ts.fujitsu.com>
2017-01-12 16:20:05 +0100

    Update README.md

--- README.md
+++ README.md
@@ -1,12 +1,25 @@
 # Ubikube
 Electron app flashing card images.
 
-### Setup
+## Setup
 Just run:
 
 ```
 git clone git@github.com:maciaszczykm/ubikube.git
 cd ubikube
 npm i
-npm start
+sudo env PATH="$PATH" npm start
 ```
+
+## Interface
+<p align="center">
+    <img src="assets/interface.png"/>
+</p>
+
+## Possible enhancements
+
+- Add build files.
+- Move from CSS to SCSS.
+- Add `cloud-init` scripts after image flash.
+- Update progress bar on image flash.
+- Read fields before flash process (currently it's hardcoded).
--- src/app/scripts/unzip.js
+++ src/app/scripts/unzip.js
@@ -2,9 +2,9 @@ export function extract7z (archive, outputdir, callback) {
   var path = require('path')
   var exec = require('child_process').exec
   if (process.platform == 'darwin') {
-    var binary = '"' + path.normalize(process.cwd() + '/7z') + '"'
+    var binary = '"' + path.normalize(process.cwd() + '/bin/7z') + '"'
   } else if (process.platform == 'win32') {
-    var binary = '"' + path.normalize(process.cwd() + '/7z.exe') + '"'
+    var binary = '"' + path.normalize(process.cwd() + '/bin/7z.exe') + '"'
   } else if (process.platform == 'linux') {
     var binary = '7za'
   }
